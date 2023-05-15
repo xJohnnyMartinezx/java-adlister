c6c3867e4257553571fa68fe36858440cb532fb6
Johnny <mrtnz.jn@gmail.com>
2023-05-15 15:50:55 -0500

    added lec notes

---
+++ src/main/java/NameServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
+public class NameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String name = req.getParameter("name");
+
+        out.println("<h1> Hello, my name is " + name + "!</h1>");
+
+
+    }
+}
