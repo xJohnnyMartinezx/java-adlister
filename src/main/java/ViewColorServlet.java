810df8a34bae46b636d5896f94399fc13e7f1654
Johnny <mrtnz.jn@gmail.com>
2023-05-18 13:41:43 -0500

    made modifications

--- src/main/java/ViewColorServlet.java
+++ src/main/java/ViewColorServlet.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewColor", urlPatterns = "/viewColor")
+@WebServlet(name = "ViewColorServlet", urlPatterns = "/viewColor")
 public class ViewColorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String color = req.getParameter("color");
+        req.setAttribute("color", color);
         req.getRequestDispatcher("/view-color.jsp").forward(req, resp);
     }
 }
