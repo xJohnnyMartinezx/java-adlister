1ea5808c3d54fa2242abec863e5a92b22444aa24
Johnny <mrtnz.jn@gmail.com>
2023-05-19 13:52:38 -0500

    completed no.1 separate concerns

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            resp.sendRedirect("/profile");
+        }
+    }
+}
