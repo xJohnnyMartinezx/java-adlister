6f1e4de3cd6945fe12cbd6118613775b6d6047f3
Johnny <mrtnz.jn@gmail.com>
2023-05-19 14:08:12 -0500

    Successfully tested login servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,11 +9,17 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         if (username.equals("admin") && password.equals("password")) {
-            resp.sendRedirect("/profile");
+            resp.sendRedirect("/profile.jsp");
+            System.out.println("Login Successful");
         }
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -10,7 +10,7 @@
     <jsp:include page="partials/navbar.jsp" />
     <div class="container">
         <h1>Please Log In</h1>
-        <form action="/login.jsp" method="POST">
+        <form action="/login" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
                 <input id="username" name="username" class="form-control" type="text">
